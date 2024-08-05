f9861cd8e01232483fc9d0f6b7c522c4ea6901b1
StephenMiner <64863697+StephenMiner@users.noreply.github.com>
2024-08-05 07:11:19 -0700

    Change loot-table roller to be % based instead of weights

--- src/main/java/me/stephenminer/raftbattle/game/fishing/FishHelper.java
+++ src/main/java/me/stephenminer/raftbattle/game/fishing/FishHelper.java
@@ -6,20 +6,19 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class FishHelper {
     private final RaftBattle plugin;
+    private final Random random;
     private List<FishingTable> tables;
     private GameMap host;
 
     public FishHelper(GameMap host){
         this.plugin = JavaPlugin.getPlugin(RaftBattle.class);
         this.host = host;
+        this.random = new Random();
         loadTables();
     }
 
@@ -43,6 +42,8 @@ public class FishHelper {
     }
 
     private FishingTable findTable(){
+        //stuff before is for when loot tables were weight baed
+        /*
         int max = tables.stream().mapToInt(FishingTable::weight).sum();
         int roll = ThreadLocalRandom.current().nextInt(max) + 1;
         for (FishingTable table : tables){
@@ -50,6 +51,22 @@ public class FishHelper {
             if (roll <= 0) return table;
         }
         return null;
+
+         */
+        List<FishingTable> pool = rollTables();
+        if (pool.isEmpty()) return null;
+        else return pool.get(random.nextInt(pool.size()));
+    }
+
+    private List<FishingTable> rollTables(){
+        List<FishingTable> tables = new ArrayList<>();
+        for (FishingTable table : this.tables){
+            int roll = random.nextInt(100);
+            if (roll < table.weight()){
+                tables.add(table);
+            }
+        }
+        return tables;
     }
 
 }
